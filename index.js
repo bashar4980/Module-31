c05626dcc1605bb716fb2a09ed8ead969359ec3e
bashar4980 <sagor.ahmed3669@gmail.com>
2022-08-25 19:59:37 +0600

    problem one is slove

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 //     numbers). [ 2, 4, 6, 8, 10 ]. Do this using for loop & array.map()
 //     method. Hints: add one to any odd number and it will become an even
 //   number.
+
+const oddNumber = [1 , 3, 5, 7, 9];
+
+function getEven(numbers){
+    const evenNumber = []
+    for(let number of numbers){
+        const result = number + 1 ;
+        evenNumber.push(result);
+
+    }
+    return evenNumber ;
+}
+const evenValue = getEven(oddNumber);
+console.log('use for loop: ',evenValue) ;
+
+const evenNumbers = oddNumber.map((number)=> number + 1);
+console.log('use map function: ' , evenNumbers)
